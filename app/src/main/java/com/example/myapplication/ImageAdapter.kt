6dda2910d64c7c3c06dd2ad74713636bc3b4f73b
yjbigbrr <youha11111@naver.com>
2024-06-28 18:29:12 +0900

    delete edit

--- app/src/main/java/com/example/myapplication/ImageAdapter.kt
+++ app/src/main/java/com/example/myapplication/ImageAdapter.kt
@@ -1,14 +1,17 @@
 import android.net.Uri
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.ViewGroup
-import com.bumptech.glide.Glide
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
 
-class ImageAdapter(private val images: ArrayList<Uri>) :
+class ImageAdapter(private val images: MutableList<Uri>) :
     RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    var isSelectMode = false
+    private val selectedImages = mutableSetOf<Uri>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false)
@@ -16,30 +19,45 @@ class ImageAdapter(private val images: ArrayList<Uri>) :
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        Glide.with(holder.itemView.context).load(images[position]).into(holder.image)
-    }
+        val uri = images[position]
+        Glide.with(holder.itemView.context).load(uri).into(holder.image)
 
-    override fun getItemCount(): Int {
-        return images.size
-    }
+        holder.itemView.setBackgroundColor(
+            if (selectedImages.contains(uri)) android.graphics.Color.BLACK else android.graphics.Color.TRANSPARENT
+        )
 
+        holder.selectButton.visibility = if (isSelectMode) View.VISIBLE else View.GONE
+        holder.selectButton.isSelected = selectedImages.contains(uri)
 
-    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val image: ImageView = itemView.findViewById(R.id.imageView)
-        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+        holder.selectButton.setOnClickListener {
+            if (holder.selectButton.isSelected) {
+                selectedImages.remove(uri)
+            } else {
+                selectedImages.add(uri)
+            }
+            notifyItemChanged(position)
+        }
 
-        init {
-            deleteButton.setOnClickListener {
-                val position = adapterPosition
-                if (position != RecyclerView.NO_POSITION) {
-                    images.removeAt(position)  // 이미지 리스트에서 해당 위치의 이미지 삭제
-                    notifyItemRemoved(position)  // 어댑터에 삭제된 것을 알림
-                }
+        holder.deleteButton.setOnClickListener {
+            if (isSelectMode) {
+                images.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, images.size)
             }
         }
     }
 
-//    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//        val image: ImageView = itemView.findViewById(R.id.imageView)
-//    }
+    override fun getItemCount(): Int = images.size
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.imageView)
+        val selectButton: ImageView = itemView.findViewById(R.id.selectButton)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+    }
+
+    fun deleteSelectedImages() {
+        images.removeAll(selectedImages)
+        selectedImages.clear()
+        notifyDataSetChanged()
+    }
 }
