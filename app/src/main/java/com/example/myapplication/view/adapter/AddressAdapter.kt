f2b4d1cb4e17f271f1c814cb987309a9dee2382b
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-01 22:09:09 +0900

    feat: 저장한 주소 삭제 기능 추가

--- app/src/main/java/com/example/myapplication/view/adapter/AddressAdapter.kt
+++ app/src/main/java/com/example/myapplication/view/adapter/AddressAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication.view.adapter
 
+import android.app.AlertDialog
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +9,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ItemAddressBinding
 
-class AddressAdapter(private val addressList: MutableList<String>, private val emptyStateTextView: TextView) : RecyclerView.Adapter<AddressAdapter.AddressViewHolder>() {
+class AddressAdapter(
+    private val addressList: MutableList<String>,
+    private val emptyStateTextView: TextView
+) : RecyclerView.Adapter<AddressAdapter.AddressViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddressViewHolder {
         val binding = ItemAddressBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -17,6 +22,9 @@ class AddressAdapter(private val addressList: MutableList<String>, private val e
     override fun onBindViewHolder(holder: AddressViewHolder, position: Int) {
         val address = addressList[position]
         holder.bind(address)
+        holder.binding.deleteButton.setOnClickListener {
+            showDeleteConfirmationDialog(holder.itemView.context, address, position)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -29,6 +37,12 @@ class AddressAdapter(private val addressList: MutableList<String>, private val e
         updateEmptyState()
     }
 
+    private fun deleteAddress(position: Int) {
+        addressList.removeAt(position)
+        notifyItemRemoved(position)
+        updateEmptyState()
+    }
+
     private fun updateEmptyState() {
         if (addressList.isEmpty()) {
             emptyStateTextView.visibility = View.VISIBLE
@@ -37,7 +51,17 @@ class AddressAdapter(private val addressList: MutableList<String>, private val e
         }
     }
 
-    class AddressViewHolder(private val binding: ItemAddressBinding) : RecyclerView.ViewHolder(binding.root) {
+    private fun showDeleteConfirmationDialog(context: Context, address: String, position: Int) {
+        AlertDialog.Builder(context)
+            .setMessage("\"$address\"을(를) 내 저장소에서 정말로 삭제하시겠어요?")
+            .setPositiveButton("예") { dialog, which ->
+                deleteAddress(position)
+            }
+            .setNegativeButton("아니요", null)
+            .show()
+    }
+
+    class AddressViewHolder(val binding: ItemAddressBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(address: String) {
             binding.address = address
