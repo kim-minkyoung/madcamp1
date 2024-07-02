6925ad96e3eaf57d5c83470335a6ffce80644590
kim-minkyoung <decomin02@pusan.ac.kr>
2024-07-02 22:21:55 +0900

    fix: 주소 검색 기능

--- app/src/main/java/com/example/myapplication/model/viewModel/MapViewModel.kt
+++ app/src/main/java/com/example/myapplication/model/viewModel/MapViewModel.kt
@@ -9,6 +9,8 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
 import java.io.IOException
+import java.net.URLEncoder
+
 class MapViewModel(application: Application) : AndroidViewModel(application) {
     private val _addressData = MutableLiveData<Address>()
     val addressData: LiveData<Address> = _addressData
@@ -32,15 +34,18 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
             .addHeader("X-NCP-APIGW-API-KEY-ID", "w86eyz5x78")
             .addHeader("X-NCP-APIGW-API-KEY", "09NHCAmjUFBTKHMuGiXvO4oY3CRYALgN5ywWfk8S")
             .build()
+
         client.newCall(request).enqueue(object : okhttp3.Callback {
             override fun onFailure(call: okhttp3.Call, e: IOException) {
                 _errorMessage.postValue("Failed to get address")
             }
+
             override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
                 response.body?.let {
                     try {
                         val jsonObject = JSONObject(it.string())
                         val addresses = jsonObject.getJSONArray("addresses")
+
                         if (addresses.length() > 0) {
                             for (i in 0 until addresses.length()) {
                                 val addressObject = addresses.getJSONObject(i)
@@ -48,14 +53,19 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
                                 val x = addressObject.getDouble("x")
                                 val y = addressObject.getDouble("y")
                                 val landName = addressObject.optString("landName", "")
+
                                 // landName이 query와 일치하는지 확인
-                                if (landName.contains(query, true)) {
-                                    _specificValue.postValue(landName)
-                                    _addressData.postValue(Address(landName, roadAddress, y, x))
-                                    return
-                                }
+//                                if (landName.contains(query, true)) {
+//                                    // landName이 일치하는 경우에만 값을 post
+//                                    _specificValue.postValue(landName)
+//                                    _addressData.postValue(Address(landName, roadAddress, y, x))
+//                                    return
+//                                } else {
+//                                    _addressData.postValue(Address(null, roadAddress, y, x))
+//                                }
+                                _addressData.postValue(Address(null, roadAddress, y, x))
                             }
-                            _errorMessage.postValue("No matching address found")
+//                            _errorMessage.postValue("No matching address found")
                         } else {
                             _errorMessage.postValue("No address found")
                         }
@@ -68,6 +78,7 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
             }
         })
     }
+
     fun reverseGeocode(lat: Double, lng: Double) {
         if (isGeocoding) return  // Prevent multiple simultaneous requests
         isGeocoding = true
@@ -97,7 +108,7 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
                                 // Extract the specific "value" field from "addition0" under "land"
                                 val land = result.getJSONObject("land")
                                 val addition0 = land.optJSONObject("addition0")
-                                val specificValue = addition0?.optString("value", "No value found") ?: "No value found"
+                                val specificValue = addition0?.optString("value", null)
                                 _specificValue.postValue(specificValue)  // Post specific value to live data
                                 _addressData.postValue(Address(specificValue, fullAddress, lat, lng))
                             }
@@ -118,6 +129,59 @@ class MapViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     fun onAddressClicked(address: Address) {
-        //todo
+        if (isGeocoding) return  // Prevent multiple simultaneous requests
+        isGeocoding = true
+
+        val client = OkHttpClient()
+        val encodedQuery = URLEncoder.encode(address.roadAddress, "UTF-8")
+        val url = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=$encodedQuery"
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("X-NCP-APIGW-API-KEY-ID", "w86eyz5x78")
+            .addHeader("X-NCP-APIGW-API-KEY", "09NHCAmjUFBTKHMuGiXvO4oY3CRYALgN5ywWfk8S")
+            .build()
+
+        client.newCall(request).enqueue(object : okhttp3.Callback {
+            override fun onFailure(call: okhttp3.Call, e: IOException) {
+                _errorMessage.postValue("Failed to geocode address")
+                isGeocoding = false
+            }
+
+            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
+                response.body?.let {
+                    try {
+                        val jsonResponse = it.string()
+                        val jsonObject = JSONObject(jsonResponse)
+                        val addresses = jsonObject.getJSONArray("addresses")
+                        if (addresses.length() > 0) {
+                            val addressObject = addresses.getJSONObject(0)
+                            val lat = addressObject.getDouble("y")
+                            val lng = addressObject.getDouble("x")
+
+                            // Move map camera to the coordinates
+
+
+                            // Show marker on the map
+//                            showMarkerOnMap(lat, lng, address)
+                        } else {
+                            _errorMessage.postValue("No address found")
+                        }
+                    } catch (e: Exception) {
+                        _errorMessage.postValue("Failed to parse response")
+                    } finally {
+                        isGeocoding = false
+                    }
+                } ?: run {
+                    _errorMessage.postValue("Response body is null")
+                    isGeocoding = false
+                }
+            }
+        })
     }
+
+    fun setAddressData(roadAddress: String, latitude: Double, longitude: Double) {
+        _addressData.value = Address(null, roadAddress, latitude, longitude)
+    }
+
+
 }
